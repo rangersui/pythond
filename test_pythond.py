54e862403252812544b65172dce7430ae0418775
Ranger Chen <78254925+rangersui@users.noreply.github.com>
2026-09-10 22:22:20 +1000

    release 0.5.2

--- test_pythond.py
+++ test_pythond.py
@@ -58,7 +58,7 @@ def section(title):
 def test_version():
     section("version")
     check("version is string", isinstance(pythond.__version__, str))
-    check("version is 0.5.1", pythond.__version__ == "0.5.1")
+    check("version is 0.5.2", pythond.__version__ == "0.5.2")
 
 
 def test_zero_dependencies():
